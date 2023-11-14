47cea5270b52f5517650b3edf258f97da285896c
毛亚琛 <maoyachen55@gmail.com>
2023-11-14 20:51:06 +0800

    vape

--- src/main.js
+++ src/main.js
@@ -1,41 +1,51 @@
-import * as mqtt from 'mqtt/dist/mqtt';
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const TAG_IMAGES = [
-  'textures/tag51-side.png',
-  'textures/tag51-side.png',
-  'textures/tag51-top.png',
-  'textures/tag51-bottom.png',
-  'textures/tag51-side.png',
-  'textures/tag51-side.png',
-];
-
-function buildTextures(modelImages) {
-  const loader = new THREE.TextureLoader();
-  return modelImages.map((img) => {
-    const texture = loader.load(img);
-    texture.generateMipmaps = false;
-    return texture;
-  });
-}
+const loader = new GLTFLoader();
+let vase = undefined;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-const tagTextures = buildTextures(TAG_IMAGES);
-const materials = tagTextures.map((texture) => new THREE.MeshBasicMaterial({ map: texture }));
-const geometry = new THREE.BoxGeometry(81.6, 2, 33.4);
-const mesh = new THREE.Mesh(geometry, materials);
-scene.add(mesh);
-const point = new THREE.PointLight(0xffffff);
-point.position.set(200, 100, 150);
+const camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+
+loader.load(
+  'models/vase.glb',
+  (gltf) => {
+    gltf.scene.position.set(0, -40, 0);
+    vase = gltf.scene;
+    scene.add(gltf.scene);
+  },
+  undefined,
+  (error) => {
+    console.error(error);
+  }
+);
+
+const environment = new RoomEnvironment();
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+scene.environment = pmremGenerator.fromScene(environment).texture;
+
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 2;
+
+let point = new THREE.DirectionalLight(0xffffff, 4);
+point.position.set(0, 200, 200);
 scene.add(point);
-const ambient = new THREE.AmbientLight(0x444444);
+
+const ambient = new THREE.AmbientLight(0xffffff, 0.26);
 scene.add(ambient);
-camera.position.set(0, 100, 100);
+
+camera.position.set(0, 100, 200);
 camera.lookAt(scene.position);
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xb9d3ff, 1);
@@ -70,14 +80,20 @@ let isStart = false;
 startButton.addEventListener('click', () => {
   if (isStart) return;
 
-  const client = mqtt.connect('ws://127.0.0.1:8083/mqtt');
-
-  client.subscribe('testtopic/#');
-
-  client.on('message', (topic, message) => {
-    const data = JSON.parse(message.toString());
-    mesh.rotation.x = data.x * (Math.PI / 180);
-    mesh.rotation.y = data.y * (Math.PI / 180);
-    mesh.rotation.z = data.z * (Math.PI / 180);
-  });
+  const client = new WebSocket('ws://127.0.0.1:8080');
+  client.onopen = function () {
+    console.log('WebSocket Client Connected');
+  };
+  client.onmessage = function (e) {
+    try {
+      const data = JSON.parse(e.data);
+      const x = data.x;
+      const y = -data.z;
+      const z = data.y;
+      const theta = Math.atan2(-y, -z);
+      const phi = Math.atan2(-x, Math.sqrt(z * z + y * y));
+      vase.rotation.z = phi;
+      vase.rotation.x = theta;
+    } catch (error) {}
+  };
 });
